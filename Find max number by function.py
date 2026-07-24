cbd829d2b17fd82cd0f304f458a3bd2f96850ff0
HRISHI9892 <h.r.jaiswal9987@gmail.com>
2026-07-24 16:42:00 +0530

    With using user define function in variable and print statement

---
+++ Find max number by function.py
@@ -0,0 +1,10 @@
+def find_max(a,b,c):
+  return max(a,b,c)
+a=15
+b=27
+c=10
+largest_number = find_max(a,b,c)
+print(f"The maximum of {a}, {b} and {c} is: {largest_number}")
+print()
+print("Other example of using user define function")
+print(f"The maximum of 5, 17 and 10 is: {find_max(5, 17, 10)}")
